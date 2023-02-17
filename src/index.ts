056e80ab025df05ad09cff1126a40d0b579e760f
Saverio <svscagn@gmail.com>
2023-02-17 19:11:48 +0100

    fix default export

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,10 @@ import * as string from "./string";
 import * as misc from "./misc";
 import * as math from "./math";
 
-export default Object.assign({}, array, string, misc, math);
+export default Object.assign({}, array, string, misc, math) as typeof array &
+  typeof string &
+  typeof misc &
+  typeof math;
 
 export * from "./array";
 export * from "./string";
