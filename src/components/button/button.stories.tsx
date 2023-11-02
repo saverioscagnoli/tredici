449d82ec519faece96a8e516389733471870f5d3
Saverio <svscagn@gmail.com>
2023-11-02 01:12:55 +0100

    add button color schemes

--- src/components/button/button.stories.tsx
+++ src/components/button/button.stories.tsx
@@ -35,13 +35,106 @@ export const Normal: Story = {
   render: () => {
     return (
       <Tredici>
-        <div style={{ display: "flex", gap: "1rem" }}>
+        <div className="flex flex-col gap-8">
           <ThemeButton />
-          <Button>Solid</Button>
-          <Button variant="secondary">Secondary</Button>
-          <Button variant="ghost">Ghost</Button>
-          <Button variant="outline">Outline</Button>
-          <Button variant="unstyled">Unstyled</Button>
+
+          <div style={{ display: "flex", gap: "1rem" }}>
+            <Button>Solid</Button>
+            <Button variant="ghost">Ghost</Button>
+            <Button variant="outline">Outline</Button>
+          </div>
+          <div style={{ display: "flex", gap: "1rem" }}>
+            <Button disabled>Solid</Button>
+            <Button disabled variant="ghost">
+              Ghost
+            </Button>
+            <Button disabled variant="outline">
+              Outline
+            </Button>
+          </div>
+
+          <div className="flex gap-4">
+            <Button colorScheme="teal">Solid</Button>
+            <Button colorScheme="teal" variant="ghost">
+              Ghost
+            </Button>
+            <Button colorScheme="teal" variant="outline">
+              Outline
+            </Button>
+          </div>
+          <div className="flex gap-4">
+            <Button disabled colorScheme="teal">
+              Solid
+            </Button>
+            <Button disabled colorScheme="teal" variant="ghost">
+              Ghost
+            </Button>
+            <Button disabled colorScheme="teal" variant="outline">
+              Outline
+            </Button>
+          </div>
+
+          <div className="flex gap-4">
+            <Button colorScheme="green">Solid</Button>
+            <Button colorScheme="green" variant="ghost">
+              Ghost
+            </Button>
+            <Button colorScheme="green" variant="outline">
+              Outline
+            </Button>
+          </div>
+          <div className="flex gap-4">
+            <Button disabled colorScheme="green">
+              Solid
+            </Button>
+            <Button disabled colorScheme="green" variant="ghost">
+              Ghost
+            </Button>
+            <Button disabled colorScheme="green" variant="outline">
+              Outline
+            </Button>
+          </div>
+
+          <div className="flex gap-4">
+            <Button colorScheme="red">Solid</Button>
+            <Button colorScheme="red" variant="ghost">
+              Ghost
+            </Button>
+            <Button colorScheme="red" variant="outline">
+              Outline
+            </Button>
+          </div>
+          <div className="flex gap-4">
+            <Button disabled colorScheme="red">
+              Solid
+            </Button>
+            <Button disabled colorScheme="red" variant="ghost">
+              Ghost
+            </Button>
+            <Button disabled colorScheme="red" variant="outline">
+              Outline
+            </Button>
+          </div>
+          <div className="flex gap-4">
+            <Button colorScheme="silver">Solid</Button>
+            <Button colorScheme="silver" variant="ghost">
+              Ghost
+            </Button>
+            <Button colorScheme="silver" variant="outline">
+              Outline
+            </Button>
+          </div>
+          <div className="flex gap-4">
+            <Button disabled colorScheme="silver">
+              Solid
+            </Button>
+            <Button disabled colorScheme="silver" variant="ghost">
+              Ghost
+            </Button>
+            <Button disabled colorScheme="silver" variant="outline">
+              Outline
+            </Button>
+          </div>
         </div>
       </Tredici>
     );
@@ -52,23 +145,123 @@ export const Icon: Story = {
   render: () => {
     return (
       <Tredici>
-        <div style={{ display: "flex", gap: "1rem" }}>
+        <div className="flex flex-col gap-8">
           <ThemeButton />
-          <Button.Icon>
-            <BsGithub />
-          </Button.Icon>
-          <Button.Icon variant="secondary">
-            <BsTwitter />
-          </Button.Icon>
-          <Button.Icon variant="ghost">
-            <BsFacebook />
-          </Button.Icon>
-          <Button.Icon variant="outline">
-            <BsTwitch />
-          </Button.Icon>
-          <Button.Icon variant="unstyled">
-            <BsLinkedin />
-          </Button.Icon>
+
+          <div style={{ display: "flex", gap: "1rem" }}>
+            <Button.Icon>
+              <BsGithub />
+            </Button.Icon>
+            <Button.Icon variant="ghost">
+              <BsTwitter />
+            </Button.Icon>
+            <Button.Icon variant="outline">
+              <BsFacebook />
+            </Button.Icon>
+          </div>
+          <div style={{ display: "flex", gap: "1rem" }}>
+            <Button.Icon disabled>
+              <BsGithub />
+            </Button.Icon>
+            <Button.Icon disabled variant="ghost">
+              <BsTwitter />
+            </Button.Icon>
+            <Button.Icon disabled variant="outline">
+              <BsFacebook />
+            </Button.Icon>
+          </div>
+
+          <div style={{ display: "flex", gap: "1rem" }}>
+            <Button.Icon colorScheme="teal">
+              <BsGithub />
+            </Button.Icon>
+            <Button.Icon colorScheme="teal" variant="ghost">
+              <BsTwitter />
+            </Button.Icon>
+            <Button.Icon colorScheme="teal" variant="outline">
+              <BsFacebook />
+            </Button.Icon>
+          </div>
+          <div style={{ display: "flex", gap: "1rem" }}>
+            <Button.Icon colorScheme="teal" disabled>
+              <BsGithub />
+            </Button.Icon>
+            <Button.Icon colorScheme="teal" disabled variant="ghost">
+              <BsTwitter />
+            </Button.Icon>
+            <Button.Icon colorScheme="teal" disabled variant="outline">
+              <BsFacebook />
+            </Button.Icon>
+          </div>
+
+          <div style={{ display: "flex", gap: "1rem" }}>
+            <Button.Icon colorScheme="green">
+              <BsGithub />
+            </Button.Icon>
+            <Button.Icon colorScheme="green" variant="ghost">
+              <BsTwitter />
+            </Button.Icon>
+            <Button.Icon colorScheme="green" variant="outline">
+              <BsFacebook />
+            </Button.Icon>
+          </div>
+          <div style={{ display: "flex", gap: "1rem" }}>
+            <Button.Icon colorScheme="green" disabled>
+              <BsGithub />
+            </Button.Icon>
+            <Button.Icon colorScheme="green" disabled variant="ghost">
+              <BsTwitter />
+            </Button.Icon>
+            <Button.Icon colorScheme="green" disabled variant="outline">
+              <BsFacebook />
+            </Button.Icon>
+          </div>
+
+          <div style={{ display: "flex", gap: "1rem" }}>
+            <Button.Icon colorScheme="red">
+              <BsGithub />
+            </Button.Icon>
+            <Button.Icon colorScheme="red" variant="ghost">
+              <BsTwitter />
+            </Button.Icon>
+            <Button.Icon colorScheme="red" variant="outline">
+              <BsFacebook />
+            </Button.Icon>
+          </div>
+          <div style={{ display: "flex", gap: "1rem" }}>
+            <Button.Icon colorScheme="red" disabled>
+              <BsGithub />
+            </Button.Icon>
+            <Button.Icon colorScheme="red" disabled variant="ghost">
+              <BsTwitter />
+            </Button.Icon>
+            <Button.Icon colorScheme="red" disabled variant="outline">
+              <BsFacebook />
+            </Button.Icon>
+          </div>
+
+          <div style={{ display: "flex", gap: "1rem" }}>
+            <Button.Icon colorScheme="silver">
+              <BsGithub />
+            </Button.Icon>
+            <Button.Icon colorScheme="silver" variant="ghost">
+              <BsTwitter />
+            </Button.Icon>
+            <Button.Icon colorScheme="silver" variant="outline">
+              <BsFacebook />
+            </Button.Icon>
+          </div>
+          <div style={{ display: "flex", gap: "1rem" }}>
+            <Button.Icon colorScheme="silver" disabled>
+              <BsGithub />
+            </Button.Icon>
+            <Button.Icon colorScheme="silver" disabled variant="ghost">
+              <BsTwitter />
+            </Button.Icon>
+            <Button.Icon colorScheme="silver" disabled variant="outline">
+              <BsFacebook />
+            </Button.Icon>
+          </div>
         </div>
       </Tredici>
     );
--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -9,26 +9,106 @@ import { VariantProps } from "class-variance-authority";
 import { cn } from "@lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center font-semibold select-none cursor-pointer transition-colors",
+  "inline-flex items-center justify-center font-semibold select-none cursor-pointer transition-colors disabled:cursor-not-allowed",
   {
     variants: {
+      colorScheme: {
+        amethyst: "amehtyst",
+        teal: "teal",
+        green: "green",
+        red: "red",
+        silver: "silver"
+      },
       variant: {
         solid:
-          "dark:bg-violet-dark bg-violet-light dark:text-black text-white dark:hover:bg-violet-darker-dark hover:bg-violet-darker-light dark:active:bg-violet-darkest-dark active:bg-violet-darkest-light",
-        secondary:
-          "dark:bg-gray-tr-dark bg-gray-tr-light dark:text-white text-black dark:hover:bg-gray-tr-2-dark hover:bg-gray-tr-2-light dark:active:bg-gray-tr-3-dark active:bg-gray-tr-3-light",
+          "dark:text-black text-white dark:bg-amethyst-400 dark:hover:bg-amethyst-300 dark:active:bg-amethyst-500 dark:disabled:bg-amethyst-800/60 bg-amethyst-500 hover:bg-amethyst-600 active:bg-amethyst-700 disabled:bg-amethyst-200",
         ghost:
-          "bg-transparent dark:text-white text-black dark:hover:bg-gray-tr-dark hover:bg-gray-tr-light dark:active:bg-gray-tr-2-dark active:bg-gray-tr-2-light",
+          "bg-transparent dark:text-amethyst-300 text-amethyst-700 dark:hover:bg-amethyst-950 dark:active:bg-amethyst-900 hover:bg-amethyst-100 active:bg-amethyst-200 dark:disabled:bg-transparent disabled:bg-transparent disabled:opacity-60",
         outline:
-          "bg-transparent border dark:border-gray-tr-2-dark border-gray-tr-2-light shadow dark:text-white text-black dark:hover:bg-gray-tr-dark hover:bg-gray-tr-light dark:active:bg-gray-tr-2-dark active:bg-gray-tr-2-light",
+          "bg-transparent border dark:border-amethyst-300 border-amethyst-700 dark:text-amethyst-300 text-amethyst-700 dark:hover:bg-amethyst-950 dark:active:bg-amethyst-900 hover:bg-amethyst-100 active:bg-amethyst-200 dark:disabled:bg-transparent disabled:bg-transparent disabled:opacity-60",
         unstyled: ""
       },
+
       size: {
         sm: "h-6 px-2 text-sm rounded-sm",
         md: "h-9 px-3 rounded-md",
         lg: "h-11 px-4 text-lg rouned-md"
       }
     },
+    compoundVariants: [
+      {
+        variant: "solid",
+        colorScheme: "teal",
+        class:
+          "dark:bg-teal-400 dark:hover:bg-teal-300 dark:active:bg-teal-500 dark:disabled:bg-teal-800/60 bg-teal-500 hover:bg-teal-600 active:bg-teal-700 disabled:bg-teal-200"
+      },
+      {
+        variant: "ghost",
+        colorScheme: "teal",
+        class:
+          "bg-transparent dark:text-teal-300 text-teal-700 dark:hover:bg-teal-950 dark:active:bg-teal-900 hover:bg-teal-100 active:bg-teal-200 dark:disabled:bg-transparent disabled:bg-transparent disabled:opacity-60"
+      },
+      {
+        variant: "outline",
+        colorScheme: "teal",
+        class:
+          "bg-transparent border dark:border-teal-300 border-teal-700 dark:text-teal-300 text-teal-700 dark:hover:bg-teal-950 dark:active:bg-teal-900 hover:bg-teal-100 active:bg-teal-200 dark:disabled:bg-transparent disabled:bg-transparent disabled:opacity-60"
+      },
+      {
+        variant: "solid",
+        colorScheme: "green",
+        class:
+          "dark:bg-green-400 dark:hover:bg-green-300 dark:active:bg-green-500 dark:disabled:bg-green-800/60 bg-green-500 hover:bg-green-600 active:bg-green-700 disabled:bg-green-200"
+      },
+      {
+        variant: "ghost",
+        colorScheme: "green",
+        class:
+          "bg-transparent dark:text-green-300 text-green-700 dark:hover:bg-green-950 dark:active:bg-green-900 hover:bg-green-100 active:bg-green-200 dark:disabled:bg-transparent disabled:bg-transparent disabled:opacity-60"
+      },
+      {
+        variant: "outline",
+        colorScheme: "green",
+        class:
+          "bg-transparent border dark:border-green-300 border-green-700 dark:text-green-300 text-green-700 dark:hover:bg-green-950 dark:active:bg-green-900 hover:bg-green-100 active:bg-green-200 dark:disabled:bg-transparent disabled:bg-transparent disabled:opacity-60"
+      },
+      {
+        variant: "solid",
+        colorScheme: "red",
+        class:
+          "dark:bg-red-400 dark:hover:bg-red-300 dark:active:bg-red-500 dark:disabled:bg-red-800/60 bg-red-500 hover:bg-red-600 active:bg-red-700 disabled:bg-red-200"
+      },
+      {
+        variant: "ghost",
+        colorScheme: "red",
+        class:
+          "bg-transparent dark:text-red-300 text-red-700 dark:hover:bg-red-950 dark:active:bg-red-900 hover:bg-red-100 active:bg-red-200 dark:disabled:bg-transparent disabled:bg-transparent disabled:opacity-60"
+      },
+      {
+        variant: "outline",
+        colorScheme: "red",
+        class:
+          "bg-transparent border dark:border-red-300 border-red-700 dark:text-red-300 text-red-700 dark:hover:bg-red-950 dark:active:bg-red-900 hover:bg-red-100 active:bg-red-200 dark:disabled:bg-transparent disabled:bg-transparent disabled:opacity-60"
+      },
+      {
+        variant: "solid",
+        colorScheme: "silver",
+        class:
+          "dark:text-white text-black dark:bg-silver-400/20 dark:hover:bg-silver-300/30 dark:active:bg-silver-500/50 dark:disabled:bg-silver-800/60 bg-silver-500/20 hover:bg-silver-600/30 active:bg-silver-700/60 disabled:bg-silver-200"
+      },
+      {
+        variant: "ghost",
+        colorScheme: "silver",
+        class:
+          "dark:text-white text-black bg-transparent dark:hover:bg-silver-950 dark:active:bg-silver-900 hover:bg-silver-100 active:bg-silver-200 dark:disabled:bg-transparent disabled:bg-transparent disabled:opacity-60"
+      },
+      {
+        variant: "outline",
+        colorScheme: "silver",
+        class:
+          "dark:text-white text-black bg-transparent border dark:border-silver-300 border-silver-700 dark:hover:bg-silver-950 dark:active:bg-silver-900 hover:bg-silver-100 active:bg-silver-200 dark:disabled:bg-transparent disabled:bg-transparent disabled:opacity-60"
+      }
+    ],
     defaultVariants: {
       variant: "solid",
       size: "md"
@@ -48,42 +128,129 @@ interface ButtonComponent
 }
 
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant = "solid", size = "md", ...props }, ref) => {
+  (
+    {
+      className,
+      colorScheme = "amethyst",
+      variant = "solid",
+      size = "md",
+      ...props
+    },
+    ref
+  ) => {
     return (
       <button
         {...props}
         ref={ref}
-        className={cn(buttonVariants({ className, variant, size }))}
+        className={cn(
+          buttonVariants({ className, colorScheme, variant, size })
+        )}
       />
     );
   }
 ) as ButtonComponent;
 
 const buttonIconVariants = cva(
-  "inline-flex items-center justify-center font-semibold select-none cursor-pointer transition-colors",
+  "inline-flex items-center justify-center font-semibold select-none cursor-pointer transition-colors disabled:cursor-not-allowed",
   {
     variants: {
+      colorScheme: {
+        amethyst: "amehtyst",
+        teal: "teal",
+        green: "green",
+        red: "red",
+        silver: "silver"
+      },
       variant: {
         solid:
-          "dark:bg-violet-dark bg-violet-light dark:text-black text-white dark:hover:bg-violet-darker-dark hover:bg-violet-darker-light dark:active:bg-violet-darkest-dark active:bg-violet-darkest-light",
-        secondary:
-          "dark:bg-gray-tr-dark bg-gray-tr-light dark:text-white text-black dark:hover:bg-gray-tr-2-dark hover:bg-gray-tr-2-light dark:active:bg-gray-tr-3-dark active:bg-gray-tr-3-light",
+          "dark:text-black text-white dark:bg-amethyst-400 dark:hover:bg-amethyst-300 dark:active:bg-amethyst-500 dark:disabled:bg-amethyst-800/60 bg-amethyst-500 hover:bg-amethyst-600 active:bg-amethyst-700 disabled:bg-amethyst-200",
         ghost:
-          "bg-transparent dark:text-white text-black dark:hover:bg-gray-tr-dark hover:bg-gray-tr-light dark:active:bg-gray-tr-2-dark active:bg-gray-tr-2-light",
+          "bg-transparent dark:text-amethyst-300 text-amethyst-700 dark:hover:bg-amethyst-950 dark:active:bg-amethyst-900 hover:bg-amethyst-100 active:bg-amethyst-200 dark:disabled:bg-transparent disabled:bg-transparent disabled:opacity-60",
         outline:
-          "bg-transparent border dark:border-gray-tr-2-dark border-gray-tr-2-light shadow dark:text-white text-black dark:hover:bg-gray-tr-dark hover:bg-gray-tr-light dark:active:bg-gray-tr-2-dark active:bg-gray-tr-2-light",
+          "bg-transparent border dark:border-amethyst-300 border-amethyst-700 dark:text-amethyst-300 text-amethyst-700 dark:hover:bg-amethyst-950 dark:active:bg-amethyst-900 hover:bg-amethyst-100 active:bg-amethyst-200 dark:disabled:bg-transparent disabled:bg-transparent disabled:opacity-60",
         unstyled: ""
       },
+
       size: {
         sm: "w-6 h-6 rounded",
         md: "w-9 h-9 rounded-md",
         lg: "w-11 h-11 rounded-md"
       }
     },
-    defaultVariants: {
-      variant: "solid",
-      size: "md"
-    }
+    compoundVariants: [
+      {
+        variant: "solid",
+        colorScheme: "teal",
+        class:
+          "dark:bg-teal-400 dark:hover:bg-teal-300 dark:active:bg-teal-500 dark:disabled:bg-teal-800/60 bg-teal-500 hover:bg-teal-600 active:bg-teal-700 disabled:bg-teal-200"
+      },
+      {
+        variant: "ghost",
+        colorScheme: "teal",
+        class:
+          "bg-transparent dark:text-teal-300 text-teal-700 dark:hover:bg-teal-950 dark:active:bg-teal-900 hover:bg-teal-100 active:bg-teal-200 dark:disabled:bg-transparent disabled:bg-transparent disabled:opacity-60"
+      },
+      {
+        variant: "outline",
+        colorScheme: "teal",
+        class:
+          "bg-transparent border dark:border-teal-300 border-teal-700 dark:text-teal-300 text-teal-700 dark:hover:bg-teal-950 dark:active:bg-teal-900 hover:bg-teal-100 active:bg-teal-200 dark:disabled:bg-transparent disabled:bg-transparent disabled:opacity-60"
+      },
+      {
+        variant: "solid",
+        colorScheme: "green",
+        class:
+          "dark:bg-green-400 dark:hover:bg-green-300 dark:active:bg-green-500 dark:disabled:bg-green-800/60 bg-green-500 hover:bg-green-600 active:bg-green-700 disabled:bg-green-200"
+      },
+      {
+        variant: "ghost",
+        colorScheme: "green",
+        class:
+          "bg-transparent dark:text-green-300 text-green-700 dark:hover:bg-green-950 dark:active:bg-green-900 hover:bg-green-100 active:bg-green-200 dark:disabled:bg-transparent disabled:bg-transparent disabled:opacity-60"
+      },
+      {
+        variant: "outline",
+        colorScheme: "green",
+        class:
+          "bg-transparent border dark:border-green-300 border-green-700 dark:text-green-300 text-green-700 dark:hover:bg-green-950 dark:active:bg-green-900 hover:bg-green-100 active:bg-green-200 dark:disabled:bg-transparent disabled:bg-transparent disabled:opacity-60"
+      },
+      {
+        variant: "solid",
+        colorScheme: "red",
+        class:
+          "dark:bg-red-400 dark:hover:bg-red-300 dark:active:bg-red-500 dark:disabled:bg-red-800/60 bg-red-500 hover:bg-red-600 active:bg-red-700 disabled:bg-red-200"
+      },
+      {
+        variant: "ghost",
+        colorScheme: "red",
+        class:
+          "bg-transparent dark:text-red-300 text-red-700 dark:hover:bg-red-950 dark:active:bg-red-900 hover:bg-red-100 active:bg-red-200 dark:disabled:bg-transparent disabled:bg-transparent disabled:opacity-60"
+      },
+      {
+        variant: "outline",
+        colorScheme: "red",
+        class:
+          "bg-transparent border dark:border-red-300 border-red-700 dark:text-red-300 text-red-700 dark:hover:bg-red-950 dark:active:bg-red-900 hover:bg-red-100 active:bg-red-200 dark:disabled:bg-transparent disabled:bg-transparent disabled:opacity-60"
+      },
+      {
+        variant: "solid",
+        colorScheme: "silver",
+        class:
+          "dark:text-white text-black dark:bg-silver-400/20 dark:hover:bg-silver-300/30 dark:active:bg-silver-500/50 dark:disabled:bg-silver-800/60 bg-silver-500/20 hover:bg-silver-600/30 active:bg-silver-700/60 disabled:bg-silver-200"
+      },
+      {
+        variant: "ghost",
+        colorScheme: "silver",
+        class:
+          "dark:text-white text-black bg-transparent dark:hover:bg-silver-950 dark:active:bg-silver-900 hover:bg-silver-100 active:bg-silver-200 dark:disabled:bg-transparent disabled:bg-transparent disabled:opacity-60"
+      },
+      {
+        variant: "outline",
+        colorScheme: "silver",
+        class:
+          "dark:text-white text-black bg-transparent border dark:border-silver-300 border-silver-700 dark:hover:bg-silver-950 dark:active:bg-silver-900 hover:bg-silver-100 active:bg-silver-200 dark:disabled:bg-transparent disabled:bg-transparent disabled:opacity-60"
+      }
+    ]
   }
 );
 
@@ -92,12 +259,23 @@ interface ButtonIconProps
     VariantProps<typeof buttonIconVariants> {}
 
 const ButtonIcon = forwardRef<HTMLButtonElement, ButtonIconProps>(
-  ({ className, variant = "solid", size = "md", ...props }, ref) => {
+  (
+    {
+      className,
+      colorScheme = "amethyst",
+      variant = "solid",
+      size = "md",
+      ...props
+    },
+    ref
+  ) => {
     return (
       <button
         {...props}
         ref={ref}
-        className={cn(buttonIconVariants({ className, variant, size }))}
+        className={cn(
+          buttonIconVariants({ className, colorScheme, variant, size })
+        )}
       />
     );
   }
