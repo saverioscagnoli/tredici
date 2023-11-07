0c331ed24c2f65d660e211003d4af2a7409821c5
Saverio <svscagn@gmail.com>
2023-11-07 16:57:47 +0100

    0.0.9

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,6 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
-import css from "rollup-plugin-import-css";
 import terser from "@rollup/plugin-terser";
 import pkg from "./package.json" assert { type: "json" };
 import postcss from "rollup-plugin-postcss";
@@ -26,7 +25,7 @@ export default [
       resolve({ browser: true, dedupe: ["react", "react-dom"] }),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
-      //terser(),
+      terser(),
       postcss(),
     ]
   },
