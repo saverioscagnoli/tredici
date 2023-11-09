4a84b5d4ea710f9e98a0008c34e9ed698d7f943f
Saverio <svscagn@gmail.com>
2023-11-09 02:50:07 +0100

    0.0.10 + font fix

--- rollup.config.js
+++ rollup.config.js
@@ -4,8 +4,9 @@ import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import terser from "@rollup/plugin-terser";
-import pkg from "./package.json" assert { type: "json" };
 import postcss from "rollup-plugin-postcss";
+import copy from "rollup-plugin-copy";
+import pkg from "./package.json" assert { type: "json" };
 
 export default [
   {
@@ -27,6 +28,12 @@ export default [
       typescript({ tsconfig: "./tsconfig.json" }),
       terser(),
       postcss(),
+      copy({
+        targets: [
+          { src: "src/assets/Inter.ttf", dest: "dist/assets" },
+          { src: "src/assets/Inter-SemiBold.ttf", dest: "dist/assets" }
+        ]
+      })
     ]
   },
   {
