75d2e4714f3225919b4dcda59ab5a16a3ed3fbbf
Saverio <svscagn@gmail.com>
2024-02-08 01:19:05 +0100

    cleaner setup

--- packages/react/.storybook/main.ts
+++ packages/react/.storybook/main.ts
@@ -1,19 +1,37 @@
 import type { StorybookConfig } from "@storybook/react-vite";
+import path from "path";
+import tsconfigPaths from "vite-tsconfig-paths";
 
+/**
+ * This function is used to resolve the absolute path of a package.
+ * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+ */
+function getAbsolutePath(value: string): any {
+  return path.dirname(require.resolve(path.join(value, "package.json")));
+}
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-interactions",
+    getAbsolutePath("@storybook/addon-links"),
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@storybook/addon-onboarding"),
+    getAbsolutePath("@storybook/addon-interactions"),
   ],
   framework: {
-    name: "@storybook/react-vite",
+    name: getAbsolutePath("@storybook/react-vite"),
     options: {},
   },
   docs: {
     autodocs: "tag",
+  },
+  viteFinal: (config) => {
+    config.plugins?.push(
+      tsconfigPaths({
+        projects: [path.resolve(__dirname, "../tsconfig.json")],
+      })
+    );
+
+    return config;
   },
 };
 export default config;
--- packages/react/src/components/button/button.stories.tsx
+++ packages/react/src/components/button/button.stories.tsx
@@ -1,105 +1,16 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { Tredici, useTheme } from "../tredici";
-import { Button } from ".";
-import React from "react";
-import { ColorScheme } from "../../types";
-import { GitHubLogoIcon } from "@radix-ui/react-icons";
+import { Button } from "../../../dist";
 
 const meta: Meta<typeof Button> = {
-  component: Button
+  title: "Components/Button",
+  component: Button,
+  tags: ["autodocs"],
 };
 
 export default meta;
+
 type Story = StoryObj<typeof Button>;
 
-/*
- *👇 Render functions are a framework specific feature to allow you control on how the component renders.
- * See https://storybook.js.org/docs/react/api/csf
- * to learn how to use render functions.
- */
-
-const ThemeButton = () => {
-  const { theme, toggle } = useTheme();
-
-  return <Button onClick={toggle}>{theme}</Button>;
-};
-
-const template = (colorScheme: ColorScheme | "gray", icon?: boolean) => {
-  let C = icon ? Button.Icon : Button;
-  return (
-    <div className="flex gap-4 mt-4">
-      <C colorScheme={colorScheme} icon={<GitHubLogoIcon />}>
-        Button
-      </C>
-      <C colorScheme={colorScheme} disabled icon={<GitHubLogoIcon />}>
-        Button
-      </C>
-
-      <C colorScheme={colorScheme} variant="outline" icon={<GitHubLogoIcon />}>
-        Button
-      </C>
-      <C
-        colorScheme={colorScheme}
-        variant="outline"
-        disabled
-        icon={<GitHubLogoIcon />}
-      >
-        Button
-      </C>
-
-      <C colorScheme={colorScheme} variant="ghost" icon={<GitHubLogoIcon />}>
-        Button
-      </C>
-      <C
-        colorScheme={colorScheme}
-        variant="ghost"
-        disabled
-        icon={<GitHubLogoIcon />}
-      >
-        Button
-      </C>
-    </div>
-  );
-};
-
-export const Primary: Story = {
-  render: () => {
-    return (
-      <Tredici>
-        <ThemeButton />
-        <div className="flex flex-col">
-          {template("amethyst")}
-          {template("teal")}
-          {template("green")}
-          {template("crimson")}
-          {template("yellow")}
-          {template("blue")}
-          {template("pink")}
-          {template("b/w")}
-          {template("gray")}
-        </div>
-      </Tredici>
-    );
-  }
-};
-
-export const Icon: Story = {
-  render: () => {
-    return (
-      <Tredici>
-        <ThemeButton />
-        <div className="flex flex-col">
-          {template("amethyst", true)}
-          {template("teal", true)}
-          {template("green", true)}
-          {template("crimson", true)}
-          {template("yellow", true)}
-          {template("blue", true)}
-          {template("pink", true)}
-          {template("b/w", true)}
-          {template("gray", true)}
-        </div>
-      </Tredici>
-    );
-  }
+export const Default: Story = {
+  render: () => <Button />,
 };
