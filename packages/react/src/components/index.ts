9c809d1351c2cbb0f6cc243d15c0a6d21634d60b
Saverio <svscagn@gmail.com>
2024-02-11 02:08:27 +0100

    add tooltip

--- packages/react/src/components/index.ts
+++ packages/react/src/components/index.ts
@@ -1,2 +1,3 @@
 export * from "./button/button";
 export * from "./tredici/tredici";
+export * from "./tooltip/tooltip";
---
+++ packages/react/src/components/tooltip/tooltip.tsx
@@ -0,0 +1,81 @@
+import { cn } from "@lib";
+import * as RxTooltip from "@radix-ui/react-tooltip";
+import React, { CSSProperties, ReactNode } from "react";
+
+import "./tooltip.css";
+
+type TooltipSide = "top" | "right" | "bottom" | "left";
+
+interface TooltipProps extends RxTooltip.TooltipProps {
+  className?: string;
+  content?: string | ReactNode;
+  withArrow?: boolean | ReactNode;
+  asChild?: boolean;
+  side?: TooltipSide;
+  sideOffset?: number;
+  style?: CSSProperties;
+}
+
+interface TooltipComponent extends React.FC<TooltipProps> {
+  Arrow: typeof TooltipArrow;
+}
+
+const Tooltip: TooltipComponent = ({
+  content,
+  withArrow = true,
+  asChild,
+  side,
+  sideOffset = 2,
+  style,
+  className,
+  delayDuration = 0,
+  defaultOpen,
+  children,
+  ...props
+}) => {
+  return (
+    <RxTooltip.Provider delayDuration={delayDuration} {...props}>
+      <RxTooltip.Root defaultOpen={defaultOpen}>
+        <RxTooltip.Trigger asChild={asChild}>{children}</RxTooltip.Trigger>
+        <RxTooltip.Portal>
+          <RxTooltip.Content
+            side={side}
+            sideOffset={sideOffset}
+            style={style}
+            className={cn(
+              "py-1 px-2 rounded-md shadow text-sm",
+              "bg-black text-white",
+              "dark:bg-white dark:text-black",
+              "tooltip-content",
+              className
+            )}
+          >
+            {content}
+            {withArrow === true ? <TooltipArrow /> : withArrow}
+          </RxTooltip.Content>
+        </RxTooltip.Portal>
+      </RxTooltip.Root>
+    </RxTooltip.Provider>
+  );
+};
+
+const TooltipArrow: React.FC<RxTooltip.TooltipArrowProps> = ({
+  width = 7,
+  height = 3,
+  className,
+  ...props
+}) => {
+  return (
+    <RxTooltip.Arrow
+      width={width}
+      height={height}
+      className={cn("fill-black", "dark:fill-white", className)}
+      {...props}
+    />
+  );
+};
+
+Tooltip.displayName = "Tooltip";
+Tooltip.Arrow = TooltipArrow;
+
+export { Tooltip };
