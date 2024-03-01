6123306c781a3ec2e5bd669da453ae11aca1c024
Saverio <svscagn@gmail.com>
2024-03-01 23:18:25 +0100

    hard reset

--- packages/react/src/components/aspect-ratio/aspect-ratio.tsx
+++
@@ -1,8 +0,0 @@
-import * as RxAspectRatio from "@radix-ui/react-aspect-ratio";
-
-type AspectRatioProps = RxAspectRatio.AspectRatioProps;
-
-const AspectRatio = RxAspectRatio.AspectRatio;
-
-export { AspectRatio };
-export type { AspectRatioProps };
